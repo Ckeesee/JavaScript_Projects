08730d514a8a29eb936f3f228bc31c7ab22f88be
Ckeesee <codman1821@gmail.com>
2021-08-14 12:31:40 -0500

    java script beginnings

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+document.write("Hello World!");
+document.write("<br>");
+window.alert("This is my alert!")
+var S = "Concatenated" + "String" + "Values";
+document.write(S);
+document.write("<br>");
+var color = "My favorite color is blue";
+var color = color.fontcolor("blue");
+document.write(color);
+document.write("<br>");
+document.write(145+9/37);
+document.write("<br>");
